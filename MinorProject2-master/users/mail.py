164de74f6c09d4560a4b742241568388e0b793f4
Sparsh17 <sparshgagneja1999@gmail.com>
2020-09-15 14:35:31 +0530

    Change in mail

--- MinorProject2-master/users/mail.py
+++ MinorProject2-master/users/mail.py
@@ -2,5 +2,5 @@ EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
 EMAIL_HOST = ‘smtp.gmail.com’
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ‘sparshgagneja1999@gmail.com’
-EMAIL_HOST_PASSWORD = ‘991452200123’
+EMAIL_HOST_USER = ‘info@gmail.com’
+EMAIL_HOST_PASSWORD = ‘*******’
